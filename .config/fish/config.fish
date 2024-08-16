36d12e62b828de3edc6a1b286d977551edb4cecf
jhiven <jivenagnar11@gmail.com>
2024-08-16 15:38:52 +0700

    add fish shell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+fish_vi_key_bindings
+
+set fish_key_bindings fish_user_key_bindings
+
+# alias
+alias mirrorupdate "echo 'updating mirrorlist' && sudo reflector --latest 5 --country 'sg,id,Australia' --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist && sudo cat /etc/pacman.d/mirrorlist"
+alias dotfiles "/usr/bin/git --git-dir='$HOME/.dotfiles/' --work-tree='$HOME'"
+
+# yazi config
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+starship init fish | source
